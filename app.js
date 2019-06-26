3810cbdd8650fede3f38a38709cc569170d04abf
marcos274 <marcosriveros660@gmail.com>
2019-06-25 20:28:38 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+//requireds
+const { crearArchivo } = require('./multiplicar/multiplicar');
+
+
+let base = '10';
+
+
+crearArchivo(base)
+    .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+    .catch(e => console.log(e));
---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,34 @@
+//requireds
+const fs = require('fs');
+
+let crearArchivo = (base) => {
+
+    return new Promise((resolve, reject) => {
+        if (!Number(base)) {
+            reject(`El valor introcido * ${ base } no es un numero`);
+            return
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= 10; i++) {
+
+            data += `${ base } * ${ i } = ${ base * i }\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${ base }.txt`, data, (err) => {
+
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla-${ base }.txt`);
+
+        });
+
+    });
+
+}
+
+module.exports = {
+    crearArchivo
+}
